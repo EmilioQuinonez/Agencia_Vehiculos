36a50b53af231cdf742ac960132c32a0a7f19a4d
EmilioQuinonez <150107914+EmilioQuinonez@users.noreply.github.com>
2024-05-31 13:13:09 -0600

    Update Coche.h

--- Coche.h
+++ Coche.h
@@ -7,13 +7,14 @@
 using namespace std;
 
 class Coche : public Vehiculo {
-    private:
-        int numPuertas;
-    public:
-        Coche();
-        Coche(string, string, int, float, int);
-        void setNumPuertas(int);
-        string getInfoVehiculo();
+private:
+    int plazas;
+
+public:
+    Coche(string, int, string, int, float);
+    void mostrarInfo() const override;
+    int getPlazas() const;
+    void setPlazas(int);
 };
 
 #endif
